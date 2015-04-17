a43b7be69dbc76fe4a32f3b9d4c14adb846e5c1a
Johannes Kulick <kulick@hildensia.de>
2015-04-17 11:11:11 +0200

    numpy test

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import lib
+import numpy as np
 
 def cb_test(x):
-    print("CB test: {}".format(x))
+    print("Python CB got following data from C++: {}".format(x))
     x[0] = 5.6
     x[1] = 7.8
     x[2] = 9.0
     return x
+
+def cb_numpy_test(x):
+    print("Before numpy: {}".format(x))
+    y = np.array(x)
+    y *= 2
+    z = list(y)
+    print("After numpy: {}".format(z))
+    return z
 
 if __name__ == "__main__":
     lib.lib_function()
@@ -13,4 +22,5 @@ if __name__ == "__main__":
     print(lib.lib_list_function([3.4, 5.6]))
     print("="*80)
     lib.lib_callback_function(cb_test)
+    lib.lib_callback_function(cb_numpy_test)
 
