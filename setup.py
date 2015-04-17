8e62b8ab9f8725c751f74a5634e7faf4590fb56a
Johannes Kulick <kulick@hildensia.de>
2015-04-17 11:11:22 +0200

    set some PATHs

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+import os
+pwd = os.getcwd()
+
+os.environ['CFLAGS'] = '-I/usr/include/python3.4m -std=c++0x'
+os.environ['LDFLAGS'] = '-L/usr/lib/python3.4m -L' + pwd + '/clib'
+
+
 setup(
     ext_modules = cythonize([Extension("lib", ["lib.pyx"],
                                        libraries=["clib"],
